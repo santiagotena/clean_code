2352a642d3881ec194b2c8ac1fc61a2997218206
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 19:53:00 +0200

    Replace parameters for choose_best_sum, change struct dtype

--- red/best_travel/best_travel.h
+++ red/best_travel/best_travel.h
@@ -11,22 +11,22 @@
 #define ERR_INVALID_MAX_DISTANCE    "Error: The maximum sum of distances must be greater than zero.\n"
 #define ERR_INVALID_TOWN_NUMBER     "Error: The number of towns to visit must be at least 1.\n"
 #define ERR_INVALID_LIST            "Error: No negative numbers are allowed in the list_of_distances\n"
+#define ERR_NOT_ENOUGH_DISTANCES    "Error: There were not enough distances provided.\n"
 
 #define VALID_INPUT_EXAMPLE         "Please provide: <maxium_sum_of_distances> <number_of_towns_to_visit> <list_of_distances>\n"
 
 //Structs//
-typedef struct s_parameters
-{
-    unsigned int    maximum_sum_of_distances;
-    unsigned int    number_of_towns_to_visit;
+typedef struct  s_parameters {
+    int    maximum_sum_of_distances;
+    int    number_of_towns_to_visit;
     int             *list_of_distances;
-
-}						t_parameters;
+    int             list_of_distances_size;
+}               t_parameters;
 
 //Functions//
 //Parsing
 bool    is_input_valid(int argc, char **argv, t_parameters *parameters);
 //Core
-int     choose_best_sum(unsigned int maximum_sum_of_distances, unsigned int number_of_towns_to_visit, int *list_of_distances);
+int     choose_best_sum(t_parameters *parameters);
 //Display Results
 void    display_results(int max_distance);
