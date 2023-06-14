fa253fac99826a8ca0335de34406d8a996891609
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 19:52:09 +0200

    Yield correct output

--- red/best_travel/is_input_valid.c
+++ red/best_travel/is_input_valid.c
@@ -8,7 +8,7 @@ bool    are_enough_arguments(int argc) {
     return (true);
 }
 
-bool    maximum_sum_of_distances_is_positive(char *argv[1], t_parameters *parameters) {
+bool    maximum_sum_of_distances_is_positive(char **argv, t_parameters *parameters) {
     parameters->maximum_sum_of_distances = atoi(argv[1]);
     if (parameters->maximum_sum_of_distances < 0) {
         printf("%s%s", ERR_INVALID_MAX_DISTANCE, VALID_INPUT_EXAMPLE);
@@ -17,7 +17,7 @@ bool    maximum_sum_of_distances_is_positive(char *argv[1], t_parameters *parame
     return (true);
 }
 
-bool    are_enough_towns_to_visit(char *argv[2], t_parameters *parameters) {
+bool    are_enough_towns_to_visit(char **argv, t_parameters *parameters) {
     parameters->number_of_towns_to_visit = atoi(argv[2]);
     if(parameters->number_of_towns_to_visit < 1) {
         printf("%s%s", ERR_INVALID_TOWN_NUMBER, VALID_INPUT_EXAMPLE);
@@ -28,7 +28,8 @@ bool    are_enough_towns_to_visit(char *argv[2], t_parameters *parameters) {
 
 void    assign_list_of_distances(int argc, char **argv, t_parameters *parameters) {
     int index = 0;
-    int list_size = argc - 3;
+    parameters->list_of_distances_size = argc - 3;
+    int list_size = parameters->list_of_distances_size;
     parameters->list_of_distances = calloc(list_size, sizeof (int));
     for (int i = 3; i < argc; i++) {
         parameters->list_of_distances[index] = atoi(argv[i]);
@@ -38,7 +39,7 @@ void    assign_list_of_distances(int argc, char **argv, t_parameters *parameters
 
 bool    distances_are_valid(int argc, char **argv, t_parameters *parameters) {
     assign_list_of_distances(argc, argv, parameters);
-    int list_size = argc - 3;
+    int list_size = parameters->list_of_distances_size;
     for (int i = 0; i < list_size; i++) {
         if (parameters->list_of_distances[i] < 0) {
             printf("%s%s", ERR_INVALID_LIST, VALID_INPUT_EXAMPLE);
@@ -50,8 +51,8 @@ bool    distances_are_valid(int argc, char **argv, t_parameters *parameters) {
 
 bool    is_input_valid(int argc, char **argv, t_parameters *parameters) {
     if (are_enough_arguments(argc) &&
-        maximum_sum_of_distances_is_positive(argv[1], parameters) &&
-        are_enough_towns_to_visit(argv[2], parameters) &&
+        maximum_sum_of_distances_is_positive(argv, parameters) &&
+        are_enough_towns_to_visit(argv, parameters) &&
         distances_are_valid(argc, argv, parameters))
         return (true);
     return (false);
