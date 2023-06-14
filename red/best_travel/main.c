74c05329641ef46187ae0a239b82f0334f881ae8
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 13:37:56 +0200

    Add pseudocode

--- red/best_travel/main.c
+++ red/best_travel/main.c
@@ -1,12 +1,15 @@
 #include "best_travel.h"
 
 int main(int argc, char **argv) {
-    t_parameters parameters;
+    t_parameters    parameters;
+    int             max_distance;
 
     if (!is_input_valid(argc, argv, &parameters))
         return (-1);
-
-
+//    max_distance = choose_best_sum(parameters.maximum_sum_of_distances,
+//                                   parameters.number_of_towns_to_visit,
+//                                   parameters.list_of_distances);
+//    display_results(max_distance);
     free(parameters.list_of_distances);
     return (0);
 }
