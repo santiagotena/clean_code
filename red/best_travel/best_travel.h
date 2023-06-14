0cd46925cc1170d7f00646a13ad6466bea6b7a50
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 11:51:48 +0200

    Add struct for parameters

--- red/best_travel/best_travel.h
+++ red/best_travel/best_travel.h
@@ -1,22 +1,33 @@
 #pragma once
 
 //Libraries//
-#include "stdio.h"
 #include "stdbool.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 //Macros//
-#define ERR_INVALID_ARGC "Error: Invalid number of arguments\n"
-#define ERR_INVALID_MAX_DISTANCE "Error: "
-#define ERR_INVALID_TOWN_NUMBER
-#define ERR_INVALID_LIST
+#define ERR_INVALID_ARGC            "Error: Invalid number of arguments\n"
+#define ERR_INVALID_MAX_DISTANCE    "Error: The maximum sum of distances must be greater than zero.\n"
+#define ERR_INVALID_TOWN_NUMBER     "Error: The number of towns to visit must be at least 1.\n"
+#define ERR_INVALID_LIST            "Error: No empty list of distances or negative numbers are allowed\n"
 
-#define VALID_INPUT_EXAMPLE "Please provide: <maxium_sum_of_distance> <number_of_towns_to_visit> <list of distances>\n"
+#define VALID_INPUT_EXAMPLE         "Please provide: <maxium_sum_of_distances> <number_of_towns_to_visit> <list_of_distances>\n"
+
+//Structs//
+typedef struct s_parameters
+{
+    unsigned int    maximum_sum_of_distances;
+    unsigned int    number_of_towns_to_visit;
+    int             *list_of_distances;
+
+}						t_parameters;
 
 //Functions//
-
 //Parsing
-bool    is_input_valid(int argc, char **argv);
+bool    is_input_valid(int argc, char **argv, t_parameters parameters);
 
 //Core
+int     choose_best_sum(unsigned int maximum_sum_of_distances, unsigned int number_of_towns_to_visit, int *list_of_distances);
 
 //Display Results
