3944adf211cb308e4cc2ca02f26330b70b65d1cf
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 10:25:00 +0200

    Add pseudo code

---
+++ red/best_travel/main.cpp
@@ -0,0 +1,15 @@
+#include "best_travel.h"
+
+int main(int argc, char **argv) {
+    //Parsing
+        //argc
+        //positive t and k
+        //ls positive and at least one element
+    //choose_best_sum();
+        //Return -1 in case of failure
+    //Print results
+        //Informs in case of failure
+        //Max distance
+        //List paired with indexes
+    return (0);
+}
