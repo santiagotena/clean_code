91dce12292e81d1b9a34995ab1feec0f4259889f
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 13:10:29 +0200

    Improve error message

--- red/best_travel/best_travel.h
+++ red/best_travel/best_travel.h
@@ -10,7 +10,7 @@
 #define ERR_INVALID_ARGC            "Error: Invalid number of arguments\n"
 #define ERR_INVALID_MAX_DISTANCE    "Error: The maximum sum of distances must be greater than zero.\n"
 #define ERR_INVALID_TOWN_NUMBER     "Error: The number of towns to visit must be at least 1.\n"
-#define ERR_INVALID_LIST            "Error: No empty list of distances or negative numbers are allowed\n"
+#define ERR_INVALID_LIST            "Error: No negative numbers are allowed in the list_of_distances\n"
 
 #define VALID_INPUT_EXAMPLE         "Please provide: <maxium_sum_of_distances> <number_of_towns_to_visit> <list_of_distances>\n"
 
