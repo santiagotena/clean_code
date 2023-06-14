8093e7755e1a03e064cd860f0ea47bbfbaf03c54
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 10:24:35 +0200

    Add base files

---
+++ red/best_travel/best_travel.h
@@ -0,0 +1,18 @@
+#pragma once
+
+//Libraries//
+#include "stdio.h"
+
+//Macros//
+#define ERR_INVALID_ARGC
+#define ERR_INVALID_MAX_DISTANCE
+#define ERR_INVALID_TOWN_NUMBER
+#define ERR_INVALID_LIST
+
+//Functions//
+
+//Parsing
+
+//Core
+
+//Display
