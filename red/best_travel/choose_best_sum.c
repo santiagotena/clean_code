010cd0f1e99a6b90a9aa50c19143c0e56305663f
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 20:43:45 +0200

    Update file

--- red/best_travel/choose_best_sum.c
+++ red/best_travel/choose_best_sum.c
@@ -1,24 +1,27 @@
 #include "best_travel.h"
 
 int     choose_best_sum(t_parameters *parameters) {
+    int     current_sum = 0;
     int     highest_sum = 0;
+//    int     *taken_indexes[parameters->number_of_towns_to_visit];
 
     if (parameters->list_of_distances_size < (int)parameters->number_of_towns_to_visit) {
         printf(ERR_NOT_ENOUGH_DISTANCES);
         return (-1);
     }
 
-//    for (int i = 0; i < number_of_towns_to_visit ; i++) {
-//        for (int j = 0; j < list_of_distances_size; ++j) {
-//            if ()
-//
-//        }
-//
-//        if (highest_sum == maximum_sum_of_distances)
-//            return (highest_sum);
-//    }
+    for (int i = 0; i < parameters->number_of_towns_to_visit ; i++) {
+        current_sum += parameters->list_of_distances[i];
 
+        for (int j = 0; j < parameters->list_of_distances_size; j++) {
+            current_sum += parameters->list_of_distances[j];
+        }
 
-    //Save indexes
+        if (highest_sum == parameters->maximum_sum_of_distances)
+            return (highest_sum);
+        if (current_sum > highest_sum)
+            highest_sum = current_sum;
+        current_sum = 0;
+    }
     return (highest_sum);
 }
