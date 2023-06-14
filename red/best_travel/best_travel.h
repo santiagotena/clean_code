3ec9175f5e20d806225af24d17a229dd0d102859
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-14 10:54:22 +0200

    Add is_input_valid.c

--- red/best_travel/best_travel.h
+++ red/best_travel/best_travel.h
@@ -2,17 +2,21 @@
 
 //Libraries//
 #include "stdio.h"
+#include "stdbool.h"
 
 //Macros//
-#define ERR_INVALID_ARGC
-#define ERR_INVALID_MAX_DISTANCE
+#define ERR_INVALID_ARGC "Error: Invalid number of arguments\n"
+#define ERR_INVALID_MAX_DISTANCE "Error: "
 #define ERR_INVALID_TOWN_NUMBER
 #define ERR_INVALID_LIST
+
+#define VALID_INPUT_EXAMPLE "Please provide: <maxium_sum _of_distance> <number_of_towns_to_visit> <list of distances>\n"
 
 //Functions//
 
 //Parsing
+bool    is_input_valid(int argc, char **argv);
 
 //Core
 
-//Display
+//Display Results
