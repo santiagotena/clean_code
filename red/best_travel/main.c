f57188e81c8dae09e8198c22fa8e9fcc37c01057
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-15 02:39:08 +0200

    Enable functions in main

--- red/best_travel/main.c
+++ red/best_travel/main.c
@@ -2,14 +2,12 @@
 
 int main(int argc, char **argv) {
     t_parameters    parameters;
-//    int             max_distance;
+    int             max_distance;
 
     if (!is_input_valid(argc, argv, &parameters))
         return (-1);
-//    max_distance = choose_best_sum(parameters);
-    choose_best_sum(&parameters);
-//    display_results(max_distance);
+    max_distance = choose_best_sum(&parameters);
+    display_results(max_distance);
     free(parameters.list_of_distances);
-
     return (0);
 }
