3ac75fa3c97ee063f60770061a887c4fe86b7931
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-16 14:20:37 +0200

    Relocate error message to display_results

--- red/best_travel/display_results.c
+++ red/best_travel/display_results.c
@@ -1,7 +1,9 @@
 #include "best_travel.h"
 
 void    display_results(int max_distance) {
-    if (max_distance == -1)
+    if (max_distance == -1) {
+        printf(ERR_NOT_ENOUGH_DISTANCES);
         return;
+    }
     printf("Max distance: %i\n", max_distance);
 }
