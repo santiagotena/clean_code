c371cf72da120c9c236499c13fc466661be751b5
Santiago Tena Hernandez <stenahe@gmail.com>
2023-06-17 12:32:25 +0200

    Modify VALID_INPUT_EXAMPLE message

--- red/best_travel/best_travel.h
+++ red/best_travel/best_travel.h
@@ -7,13 +7,13 @@
 #include "string.h"
 
 //Macros//
-#define ERR_INVALID_ARGC            "Error: Invalid number of arguments\n"
+#define ERR_INVALID_ARGC            "Error: Invalid number of arguments.\n"
 #define ERR_INVALID_MAX_DISTANCE    "Error: The maximum sum of distances must be at least zero.\n"
 #define ERR_INVALID_TOWN_NUMBER     "Error: The number of towns to visit must be at least 1.\n"
-#define ERR_INVALID_LIST            "Error: No negative numbers are allowed in the distances\n"
+#define ERR_INVALID_LIST            "Error: No negative numbers are allowed in the distances list.\n"
 #define ERR_NOT_ENOUGH_DISTANCES    "Error: There were not enough distances provided.\n"
 
-#define VALID_INPUT_EXAMPLE         "Please provide: <maximum_sum_of_distances> <number_of_towns_to_visit> <distances_list>\n"
+#define VALID_INPUT_EXAMPLE         "Please input:\n./best_travel <maximum_sum_of_distances> <number_of_towns_to_visit> <distances_list>\n"
 
 //Structs//
 typedef struct  s_input {
