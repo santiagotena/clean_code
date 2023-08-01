76c00fd0faaecb7bf24ce975faccb21214d99ff7
Santiago Tena Hernandez <stenahe@gmail.com>
2023-08-02 00:37:32 +0200

    Add tests to do list

---
+++ orange/assembler-interpreter/ts/main.test.ts
@@ -0,0 +1,25 @@
+it('test', () => {
+    expect(2).toBe(2);
+});
+
+it.todo('move');
+
+it.todo('increase');
+
+it.todo('decrease');
+
+it.todo('move & increase');
+
+it.todo('move & decrease');
+
+it.todo('jump back by integer');
+
+it.todo('jump back by register');
+
+it.todo('jump back - out of bounds');
+
+it.todo('jump forward by integer');
+
+it.todo('jump forward by register');
+
+it.todo('jump forward - out of bounds');
